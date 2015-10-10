a736ae1cec65a03fb3f90f358a28074492600d9b
andy <andy@andydote.co.uk>
2015-10-10 12:07:25 +0100

    inMemory: implement rabbitmq routing keys

---
+++ Core.Tests/Memory/MemoryListenerTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Core.Memory;
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Core.Tests.Memory
+{
+	public class MemoryListenerTests
+	{
+		private readonly ITestOutputHelper _output;
+
+		public MemoryListenerTests(ITestOutputHelper output)
+		{
+			_output = output;
+		}
+
+		[Theory]
+		[InlineData("Some.Matching.Pattern", "Some.Matching.Pattern", true)]
+		[InlineData("Some.Matching.Pattern", "Some.NonMatching.Key", false)]
+		[InlineData("Some.*.Pattern", "Some.Matching.Pattern", true)]
+		[InlineData("Some.*.Pattern", "Some.NonMatching.Key", false)]
+		[InlineData("Some.*.Pattern", "Some.NonMatching.Pattern.Key", false)]
+		[InlineData("Some.#.Pattern", "Some.NonMatching.Pattern.Key", false)]
+		[InlineData("NYSE.TECH.MSFT", "NYSE.TECH.MSFT", true)]
+		[InlineData("#", "NYSE.TECH.MSFT", true)]
+		[InlineData("NYSE.#", "NYSE.TECH.MSFT", true)]
+		[InlineData("*.*", "NYSE.TECH.MSFT", false)]
+		[InlineData("NYSE.*", "NYSE.TECH.MSFT", false)]
+		[InlineData("NYSE.TECH.*", "NYSE.TECH.MSFT", true)]
+		[InlineData("NYSE.*.MSFT", "NYSE.TECH.MSFT", true)]
+		public void TheoryMethodName(string pattern, string key, bool matches)
+		{
+			//nyse.tech.msft taken from:
+			//http://spring.io/blog/2010/06/14/understanding-amqp-the-protocol-used-by-rabbitmq/
+
+			MemoryListener.Matches(pattern, key).ShouldBe(matches);
+		}
+	}
+}
--- Core/Memory/MemoryListener.cs
+++ Core/Memory/MemoryListener.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Core.Memory
 {
@@ -23,6 +25,61 @@ namespace Core.Memory
 
 			_onReceive(message);
 		}
+
+		public static bool Matches(string routingPattern, string messageRoutingKey)
+		{
+			var parts = routingPattern.Split('.');
+			var s = new Segment(parts.First());
+			var root = s;
+
+			foreach (var source in parts.Skip(1))
+			{
+				s = s.SetChild(new Segment(source));
+			}
+
+			return root.IsMatch(messageRoutingKey.Split('.'));
+
+		}
+
+		private class Segment
+		{
+			private string _pattern;
+			private Segment _child;
+
+			public Segment(string partPattern)
+			{
+				_pattern = partPattern;
+			}
+
+			public Segment SetChild(Segment child)
+			{
+				_child = child;
+				return child;
+			}
+
+			public bool IsMatch(IEnumerable<string> parts)
+			{
+				var first = parts.First();
+				var remaining = parts.Skip(1);
+
+				if (_pattern == "#" && _child == null)
+				{
+					return true;
+				}
+
+				if (first == _pattern || (_pattern == "*" && Regex.IsMatch(first, "^.*$")))
+				{
+					if (_child != null && remaining.Any())
+						return _child.IsMatch(remaining);
+
+					return _child == null && remaining.Any() == false;
+				}
+
+				return false;
+			}
+		}
+
+
 
 		public void Dispose()
 		{
