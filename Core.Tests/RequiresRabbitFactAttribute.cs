752e7f907b75ffb3f9b465cbdef4de57975095e8
andy <andy@andydote.co.uk>
2015-10-11 14:56:41 +0100

    tests: add requiresRabbitFact attribute

---
+++ Core.Tests/RequiresRabbitFactAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Configuration;
+using RabbitMQ.Client;
+using Xunit;
+
+namespace Core.Tests
+{
+	public class RequiresRabbitFactAttribute : FactAttribute
+	{
+		public RequiresRabbitFactAttribute()
+		{
+			if (IsRabbitAvailable.Value == false)
+			{
+				Skip = "RabbitMQ is not available";
+			}
+		}
+
+		private static readonly Lazy<bool> IsRabbitAvailable;
+
+		static RequiresRabbitFactAttribute()
+		{
+			IsRabbitAvailable = new Lazy<bool>(() =>
+			{
+				var factory = new ConnectionFactory
+				{
+					HostName = ConfigurationManager.AppSettings["RabbitHost"],
+					RequestedConnectionTimeout = 1000
+				};
+
+				try
+				{
+					using (var connection = factory.CreateConnection())
+					{
+						return connection.IsOpen;
+					}
+				}
+				catch (Exception)
+				{
+					return false;
+				}
+			});
+		}
+	}
+}
