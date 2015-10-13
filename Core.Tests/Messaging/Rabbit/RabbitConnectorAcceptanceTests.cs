10316e2c8239141a56d7ad0f69b3f66da2c57474
andy <andy@andydote.co.uk>
2015-10-13 20:15:54 +0100

    tests: add output to help with debugging rabbitmq tests

--- Core.Tests/Messaging/Rabbit/RabbitConnectorAcceptanceTests.cs
+++ Core.Tests/Messaging/Rabbit/RabbitConnectorAcceptanceTests.cs
@@ -4,15 +4,18 @@ using System.Threading;
 using Core.Messaging.Rabbit;
 using Shouldly;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Core.Tests.Messaging.Rabbit
 {
 	public class RabbitConnectorAcceptanceTests
 	{
+		private readonly ITestOutputHelper _output;
 		private readonly RabbitConnector _connector;
 
-		public RabbitConnectorAcceptanceTests()
+		public RabbitConnectorAcceptanceTests(ITestOutputHelper output)
 		{
+			_output = output;
 			_connector = new RabbitConnector(ConfigurationManager.AppSettings["RabbitHost"]);
 		}
 
@@ -64,24 +67,27 @@ namespace Core.Tests.Messaging.Rabbit
 		public void When_doing_multiple_rpc()
 		{
 			var count = 0;
-			_connector.SubscribeTo<TestMessage>("TestQueue", (c, m) =>
+			_connector.SubscribeTo<TestMessage>("MutliQueue", (c, m) =>
 			{
+				_output.WriteLine("SubscribeTo.Respond {0}", count);
 				c.CanRespond().ShouldBe(true);
 				c.RespondWith(new TestResponse { Reply = count++ });
 			});
 
-			var publisher = (RabbitMessagePublisher)_connector.CreatePublisher("TestQueue");
+			var publisher = (RabbitMessagePublisher)_connector.CreatePublisher("MutliQueue");
 			var wait1 = new AutoResetEvent(false);
 			var wait2 = new AutoResetEvent(false);
 
 			publisher.Query<TestResponse>(new TestMessage { Name = "Andy Dote" }, m =>
 			{
+				_output.WriteLine("Query.0");
 				m.Reply.ShouldBe(0);
 				wait1.Set();
 			});
 
 			publisher.Query<TestResponse>(new TestMessage { Name = "Andy Dote" }, m =>
 			{
+				_output.WriteLine("Query.1");
 				m.Reply.ShouldBe(1);
 				wait2.Set();
 			});
