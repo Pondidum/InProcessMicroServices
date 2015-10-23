7f1c574971a89bdcbdee5489b719e8ce654cb38a
andy <andy@andydote.co.uk>
2015-10-23 18:56:10 +0100

    plugins: add an echo plugin

---
+++ EchoPlugin/EchoComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Core;
+using Core.Messaging;
+
+namespace EchoPlugin
+{
+	public class EchoComponent : IPluginComponent
+	{
+		public void Initialise(IQueueConnector connector)
+		{
+			connector.SubscribeTo<MessageDto>("echo", (r, args) =>
+			{
+				Console.WriteLine("[{0}]EchoPlugin Recieved: {1}", AppDomain.CurrentDomain.FriendlyName, args.Message);
+				r.RespondWith(new MessageDto { Message = "Echo: " + args.Message });
+			});
+		}
+
+		private class MessageDto
+		{
+			public string Message { get; set; }
+		}
+	}
+}
